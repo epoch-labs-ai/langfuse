71c1b0b2c7f2d7a365aabf19dab025508be31051
Sahil <kumar.sahil@gmail.com>
2024-01-12 21:25:21 +0530

    WIP

---
+++ src/features/public-api/components/CreateEpochApiKeyButton.tsx
@@ -0,0 +1,56 @@
+import { Button } from "@/src/components/ui/button";
+import * as Dialog from "@radix-ui/react-dialog";
+import { PlusIcon } from "lucide-react";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+
+interface ApiResponse {
+  success: boolean;
+}
+
+export function CreateEpochApiKeyButton(props: { projectId: string }) {
+  const [open, setOpen] = useState(false);
+
+  async function createEpochApiKey() {
+    console.log("onSubmit triggered");
+
+    try {
+      const postData = {
+        project_id: props.projectId,
+      };
+
+      const response = await fetch("/epochApiKey", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const result = (await response.json()) as ApiResponse;
+      setOpen(false);
+    } catch (error) {
+      console.log("Submission error:", error);
+    }
+  }
+
+  return (
+    <Dialog.Root open={open} onOpenChange={createEpochApiKey}>
+      <Dialog.Trigger asChild>
+        <Button variant="secondary">
+          <PlusIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
+          Create Epoch API Key
+        </Button>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="dialog-overlay-styles" />
+        <Dialog.Content className="dialog-content-styles">
+          <Dialog.Title className="mb-5 font-bold">Create API Key</Dialog.Title>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+}
---
+++ src/features/public-api/components/EpochApiKeyList.tsx
@@ -0,0 +1,67 @@
+import { Card } from "@/src/components/ui/card";
+import {
+  Table,
+  TableHeader,
+  TableRow,
+  TableHead,
+} from "@/src/components/ui/table";
+import { CreateEpochApiKeyButton } from "@/src/features/public-api/components/CreateEpochApiKeyButton";
+import { TableBody } from "@tremor/react";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+
+export function EpochApiKeyList(props: { projectId: string }) {
+  interface EpochApiKey {
+    created: Date;
+    api_key: string;
+  }
+
+  const [epochApiKey, setEpochApiKey] = useState<EpochApiKey | null>(null);
+
+  async function getEpochApiKeys() {
+    try {
+      console.log("Requesting Epoch API Keys");
+      const response = await fetch("/apiKeys", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          // TODO auth header
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error. Status ${response.status}`);
+      }
+
+      const result = (await response.json()) as EpochApiKey;
+      setEpochApiKey(result);
+    } catch (error) {
+      console.error("Failed to retrieve Epoch API keys", error);
+    }
+  }
+
+  return (
+    <div>
+      <h2 className="mb-5 text-base font-semibold leading-6 text-gray-900">
+        Epoch API Keys
+      </h2>
+      <Card className="mb-4">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="hidden text-gray-900 md:table-cell">
+                Created
+              </TableHead>
+              <TableHead className="text-gray-900">Key</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody className="text-gray-500">
+            <TableRow></TableRow>
+          </TableBody>
+        </Table>
+      </Card>
+      <div>{JSON.stringify(useSession())}</div>
+      <CreateEpochApiKeyButton projectId={props.projectId} />
+    </div>
+  );
+}
